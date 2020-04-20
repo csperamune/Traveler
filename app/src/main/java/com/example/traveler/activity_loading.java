840acafb474a432e8e0760f66a44c8add6d4b51b
Chamith <csperamune@gmail.com>
2020-04-20 07:30:02 +0530

    Test Firebase connection
    Connection is successful

--- app/src/main/java/com/example/traveler/activity_loading.java
+++ app/src/main/java/com/example/traveler/activity_loading.java
@@ -3,6 +3,7 @@ package com.example.traveler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class activity_loading extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class activity_loading extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+        Toast.makeText(activity_loading.this, "Firebase connection Success", Toast.LENGTH_LONG).show();
     }
 }
