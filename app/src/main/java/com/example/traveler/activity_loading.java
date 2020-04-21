5d959abb6527aa4b0ee2d061ed716a28c388debe
Chamith <csperamune@gmail.com>
2020-04-21 13:08:13 +0530

    upload

--- app/src/main/java/com/example/traveler/activity_loading.java
+++ app/src/main/java/com/example/traveler/activity_loading.java
@@ -11,5 +11,6 @@ public class activity_loading extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+        Toast.makeText(activity_loading.this, "Firebase connection Success", Toast.LENGTH_LONG).show();
     }
 }
